0825ec389f5b94b657b6206d5abdc851b45f73fa
underwhelmed-ape <james.ulyett@gmail.com>
2018-12-20 17:06:59 +0000

    start getting recipe urls from pages

--- __main__.py
+++ __main__.py
@@ -1,10 +1,8 @@
 
 import extract
+import transform
 import os
 
-
-# for page get get_html
-#
 start_page = 1
 end_page = 1
 
@@ -13,11 +11,12 @@ recipe_page = start_page
 # create empty list for individual recipe urls to be saved in
 recipe_links = []
 
-# local path ofr xml files to be saved in
+# local path for xml files to be saved in
 output_path = ''
 print(output_path)
 
 while recipe_page <= end_page:
+# go to page and get raw html code
     url = 'https://www.brewtoad.com/recipes?page={page}&sort=rank'.format(page=recipe_page)
     response = extract.get_html(url)
 
@@ -25,13 +24,17 @@ while recipe_page <= end_page:
     print(type(response))
     print(url)
 
+# inside the html, get the links for each of the recipes pages
+    recipe_links = transform.get_recipe_urls_from_html(response)
+    recipe_links = recipe_links.append()
 
+# increase page number by one before returning to start of loop
     recipe_page = recipe_page + 1
 
 
 
-# go to page and get raw html code
-# inside the html, get the links for each of the recipes pages
+
+
 # for each recipe page, get the get_html
 # in each recipe html, find the xml file download link
 # download the xml into output_path
