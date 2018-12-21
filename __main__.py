aadf08e6f985f08085a1978bd200cec6220e0c4a
underwhelmed-ape <james.ulyett@gmail.com>
2018-12-21 15:02:05 +0000

    update transform

--- __main__.py
+++ __main__.py
@@ -5,30 +5,48 @@ import os
 start_page = 1
 end_page = 1
 
-recipe_page = start_page
+current_recipe_page = start_page
 
 # create empty list for individual recipe urls to be saved in
 recipe_links = []
 
 # local path for xml files to be saved in
-output_path = ''
-print(output_path)
+#print(output_path)
 
-while recipe_page <= end_page:
-# go to page and get raw html code
-# if html file, will return its content
-    url = 'https://www.brewtoad.com/recipes?page={page}&sort=rank'.format(page=recipe_page)
-    response = extract.get_html(url)
-    print(len(response))
-# inside the html, get the links for each of the recipes pages
-    recipe_links = transform.get_recipe_urls_from_html(response)
-    #recipe_links = recipe_links.append()
+# Loop through each `all_recipe_pages`
+while current_recipe_page <= end_page:
+
+    # add current recipe page to url
+    all_recipe_pages = 'https://www.brewtoad.com/recipes?page={page}&sort=rank'.format(page=current_recipe_page)
+
+    # at each page use `get request` to bring back raw html
+    response = extract.get_html(all_recipe_pages)
+
+    # extract links to each recipe from recipe-container
+    recipe_link = 'test'
+    # append this information to recipe_links list
+    recipe_links = recipe_links.append(recipe_link)
+
+    for i in recipe_links:
+        output_path = 'base_path/{i}'.format(i)
+        if not os.path.isfile(output_path): # if file is not in the given dir
+            print('Downloading data at ' + output_path)
+            try:
+                # bring back
+                extract.get_html()
+                # inside the html, get the links for each of the recipes pages
+                recipe_links = transform.get_recipe_urls_from_html(response)
+                transform.save_output_to_file(response, output_path)
+            except:
+                print('Recipe file for ' + str(i) + ' not found')
+        else: # if file is in given dir
+            print('File already exists')
 
 # increase page number by one before returning to start of loop
-    recipe_page = recipe_page + 1
+current_recipe_page = current_recipe_page + 1
 
 
-
+print(recipe_links)
 
 
 # for each recipe page, get the get_html
--- transform.py
+++ transform.py
@@ -1,18 +1,17 @@
-# from the raw html files get thw information
+# from the raw html files get the information
 import extract
 
 from bs4 import BeautifulSoup
+from bs4 import SoupStrainer
 
-# at the moent this is just one htmls_list
+# at the moment this is just one htmls_list
 # will need to loop through a list of htmls
 
 htmls_list = extract.get_html('https://www.brewtoad.com/recipes?page=1&sort=rank')
-html = BeautifulSoup(htmls_list, 'html.parser')
 
+def get_recipe_urls_from_html(htmls_list):
+    for link in BeautifulSoup(htmls_list, parse_only=SoupStrainer('a')):
+        if link.has_attr('href'):
+            print(link['href'])
 
-def get_recipe_urls_from_html(html):
-    for i in html.select('a'):
-        if i['class'] == 'recipe-link':
-            print(i.text)
-
-get_recipe_urls_from_html(html)
+get_recipe_urls_from_html(htmls_list)
